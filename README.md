a0d1b41a711bf145192419009171c267470b2a11
thanhbv <thanhbv@sandinh.net>
2017-10-25 23:08:21 +0700

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# module-files-patch
+
+fetch some files from your fork of some packages then overwrite the original files in node_modules
+
+### install
+```bash
+npm i -D module-files-patch
+```
+
+### usage
+Example:
+Your need change some typescript definition types for package angular,
+then you fork [DefinitelyTyped](https://github.com/DefinitelyTyped/DefinitelyTyped) & change file `jqlite.d.ts`
+
+You should create a PR to DefinitelyTyped.
+But before the PR is merged, you can use this package to download your changed files & overwrite the original files by
+adding npm `postinstall` script to your `package.json`, ex:
+```json
+{
+  "scripts": {
+    "postinstall": "node module-files-patch cropperjs,20560,index angular,index,jqlite"
+  }
+}
+```
+
+Here:
++ `cropperjs`, `angular` is the packages.
++ `20560` (optional) is the [PR number](https://github.com/DefinitelyTyped/DefinitelyTyped/pull/20560)
++ `index`, `jqlite` is filenames in the package. The following files will be fetch (from your fork) & overwrite:
+    + `node_modules/@types/cropperjs/index`
+    + `node_modules/@types/angular/{index, jqlite}`
---
+++ index.ts
@@ -0,0 +1,43 @@
+// patching types
+
+// usage: ts-node index.ts <pkg>,[pull_request_number,]<file1>,<file2>
+// ex: ts-node index.ts cropperjs,20560,index
+// see npm script `prepare` in package.json
+import * as http from 'https';
+import * as fs from 'fs';
+
+const GithubOrg = "ohze";
+
+function save(url, path) {
+    const ws = fs.createWriteStream(path);
+    console.log(`fetching ${url}`);
+    ws.on("finish", () => console.log(`saved ${path}`));
+    http.get(url, res => res.pipe(ws));
+}
+
+function fetchType(pkg: string, file: string) {
+    const path = `types/${pkg}/${file}.d.ts`;
+    // when change types in package `pkg`, we should push to branch `pkg`
+    save(`https://raw.githubusercontent.com/${GithubOrg}/DefinitelyTyped/${pkg}/${path}`,
+        `./node_modules/@${path}`);
+}
+
+/**
+ * @param {string} pkg package name, ex "paho-mqtt"
+ * @param {string[]} files array of files that are changed/ added, ex ["index", "module"]
+ */
+function fetchTypes(pkg: string, files: string[]) {
+    files.forEach(f => fetchType(pkg, f));
+}
+
+for (let i = 2; i < process.argv.length; i++) {
+    const arg = process.argv[i];
+    let [pkg, ...files] = arg.split(","); // tslint:disable-line prefer-const
+    let prNum: string | number;
+    if (files.length > 0 && /\d+/.test(files[0])) {
+        [prNum, ...files] = files;
+        prNum = parseInt(prNum, 10);
+        console.log(`TODO check https://github.com/DefinitelyTyped/DefinitelyTyped/pull/${prNum}`);
+    }
+    fetchTypes(pkg, files);
+}
